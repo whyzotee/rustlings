2099d7fa76d226886a5f8da499cb4e9563b5692c
Chatnarint Boonsaeng <zenlektomyum@gmail.com>
2025-07-07 15:40:49 +0700

    ✨ feat: ex7-9

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -1,9 +1,11 @@
 fn trim_me(input: &str) -> &str {
     // TODO: Remove whitespace from both ends of a string.
+    input.trim()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There are multiple ways to do this.
+    let s = input + " world!";
 }
 
 fn replace_me(input: &str) -> String {
