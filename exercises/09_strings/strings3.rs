7c554e7f44b79f8555115011742e2b2b259c4f6a
Chatnarint Boonsaeng <zenlektomyum@gmail.com>
2025-07-24 13:19:26 +0700

    ✨ feat: ex09-11

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -5,11 +5,12 @@ fn trim_me(input: &str) -> &str {
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There are multiple ways to do this.
-    let s = input + " world!";
+    format!("{input} world!")
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons".
+    input.replace("cars", "balloons")
 }
 
 fn main() {
