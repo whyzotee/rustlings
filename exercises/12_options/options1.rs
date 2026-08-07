2b4c45723c58f056338c19a67bd9e3751a6d7218
Chatnarint Boonsaeng <zenlektomyum@gmail.com>
2026-08-07 11:39:41 +0700

    ex12 options

--- exercises/12_options/options1.rs
+++ exercises/12_options/options1.rs
@@ -4,6 +4,13 @@
 // `hour_of_day` is higher than 23.
 fn maybe_ice_cream(hour_of_day: u16) -> Option<u16> {
     // TODO: Complete the function body.
+    if hour_of_day > 23 {
+        None
+    } else if hour_of_day >= 22 {
+        Some(0)
+    } else {
+        Some(5)
+    }
 }
 
 fn main() {
@@ -19,6 +26,7 @@ mod tests {
         // TODO: Fix this test. How do you get the value contained in the
         // Option?
         let ice_creams = maybe_ice_cream(12);
+        let ice_creams = ice_creams.unwrap();
 
         assert_eq!(ice_creams, 5); // Don't change this line.
     }
