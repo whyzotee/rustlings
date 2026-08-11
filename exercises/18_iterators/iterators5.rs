efaa754f4b0ab8f445f3b5cfde6d8bc0170ba342
Chatnarint Boonsaeng <zenlektomyum@gmail.com>
2026-08-11 15:38:38 +0700

    ex18 iterators 4,5

--- exercises/18_iterators/iterators5.rs
+++ exercises/18_iterators/iterators5.rs
@@ -28,6 +28,12 @@ fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
 fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
     // `map` is a hash map with `String` keys and `Progress` values.
     // map = { "variables1": Complete, "from_str": None, … }
+    map.iter().fold(0, |mut c, p_value| {
+        if *p_value.1 == value {
+            c += 1;
+        }
+        c
+    })
 }
 
 fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
@@ -48,6 +54,10 @@ fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Pr
     // `collection` is a slice of hash maps.
     // collection = [{ "variables1": Complete, "from_str": None, … },
     //               { "variables2": Complete, … }, … ]
+    collection.iter().fold(0, |mut c, m| {
+        c += count_iterator(m, value);
+        c
+    })
 }
 
 fn main() {
