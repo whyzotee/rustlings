f20311caca82da973e4d55327639415afe894784
Chatnarint Boonsaeng <zenlektomyum@gmail.com>
2026-08-22 16:48:33 +0700

    finish yaa ex23 from_str, try_from_into, as_ref_mut

--- exercises/23_conversions/from_str.rs
+++ exercises/23_conversions/from_str.rs
@@ -41,7 +41,28 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {}
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let split: Vec<&str> = s.split(',').collect();
+
+        if split.len() != 2 {
+            return Err(ParsePersonError::BadLen);
+        }
+
+        let name = split[0].to_string();
+
+        if name.is_empty() {
+            return Err(ParsePersonError::NoName);
+        }
+
+        let age = match split[1].parse::<u8>() {
+            Ok(value) => value,
+            Err(e) => {
+                return Err(ParsePersonError::ParseInt(e));
+            }
+        };
+
+        Ok(Self { name, age })
+    }
 }
 
 fn main() {
